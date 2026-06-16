1ae13d27d76a471bc0c8a8e9af49953554e434d0
Matheus Koehler Zanella <matheuszanella.sma@gmail.com>
2026-06-16 20:30:01 -0300

    adicionndo cypress cloud

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,7 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   allowCypressEnv: false,
+  projectId: "4ngizh",
 
   e2e: {
     baseUrl: 'https://restful-booker.herokuapp.com',
