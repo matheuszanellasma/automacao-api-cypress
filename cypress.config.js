cafd629277f03f090e2380a7dd186a2a7201607b
Matheus Koehler Zanella <matheuszanella.sma@gmail.com>
2026-06-26 19:50:39 -0300

    atualizando pipeline

--- cypress.config.js
+++ cypress.config.js
@@ -2,10 +2,10 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
     reporter: 'mochawesome',
+    allowCypressEnv: false,
+    projectId: "4ngizh",
     e2e: {
         baseUrl: 'https://restful-booker.herokuapp.com',
-        allowCypressEnv: false,
-        projectId: "4ngizh",
         setupNodeEvents(on, config) {
 
         },
